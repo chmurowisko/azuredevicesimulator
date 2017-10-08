58b3b2a348bbf6631f1a7cf636a11e3d31d9908b
Damian Mazurek <damian.s.mazurek@gmail.com>
2017-10-08 09:48:06 +0200

    Configuration update
    Update of configuration files

--- app.constants.ts
+++ app.constants.ts
@@ -1,4 +1,4 @@
 export class Constants{
-    static readonly DeviceConnectionString = "HostName=dmiotdevhub.azure-devices.net;DeviceId=DEVICE_IOTDEV;SharedAccessKey=e7g6M3Unp5kTb8/EDC7mRbVjjnYrZFVwJq+lVop7MBE=";
-    static readonly FilePath = "C:\\Users\\THINK\\Desktop\\fileToUpload.png";
+    static readonly DeviceConnectionString = "";
+    static readonly FilePath = "";
 }
